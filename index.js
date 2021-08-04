ea94606e1da0159a9670cf127716df367b486872
shanecolby <shanecolbyzero8@gmail.com>
2021-08-04 16:35:55 -0500

    show player value while switching players each click

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ rollBtn.addEventListener("click", function () {
   const randomNumber = Math.floor(Math.random() * 6) + 1
 
   console.log(randomNumber)
+
+  if (player1Turn) {
+    console.log("player 1 rolled " + randomNumber)
+  } else {
+    console.log("player 2 rolled " + randomNumber)
+  }
+  player1Turn = !player1Turn
+
 })
 
